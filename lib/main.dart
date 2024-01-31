b5980a80b8a8b7dba8a738990bfb0c75f97358e4
日野義斗 <hino.akito@nck.co.jp>
2024-01-31 21:28:36 +0900

    :🍻: ListViewを利用した表示

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
@@ -31,92 +30,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+  final List<Map<String, String>> beers = [
+    {"name": "Beer Name 1", "brewery": "Brewery 1", "style": "Style 1"},
+    {"name": "Beer Name 2", "brewery": "Brewery 2", "style": "Style 2"},
+    {"name": "Beer Name 3", "brewery": "Brewery 3", "style": "Style 3"},
+    {"name": "Beer Name 4", "brewery": "Brewery 4", "style": "Style 4"},
+    {"name": "Beer Name 5", "brewery": "Brewery 5", "style": "Style 5"},
+    // 他のビールデータをここに追加していく
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 106,
-              color: Colors.grey[700],
-              child: Row(
-                children: [
-                  Container(
-                    width: 80,
-                    height: 80,
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: AspectRatio(
-                        // 正方形の比率を設定
-                          aspectRatio: 1.0,
-                          child: SvgPicture.asset(
-                              'assets/images/beer_sample.svg',
-                              fit: BoxFit.cover
-                          )
+      body: ListView.builder(
+        itemCount: beers.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.infinity,
+            height: 106,
+            color: Colors.grey[700],
+            child: Row(
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: AspectRatio(
+                      // 正方形の比率を設定
+                        aspectRatio: 1.0,
+                        child: SvgPicture.asset(
+                            'assets/images/beer_sample.svg',
+                            fit: BoxFit.cover
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        beers[index]["name"]!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Noto Sans',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
+                      Text(
+                        beers[index]["brewery"]!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Noto Sans',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        beers[index]["style"]!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Noto Sans',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Beer Name",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontFamily: 'Noto Sans',
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Text(
-                          "Brewery",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontFamily: 'Noto Sans',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          "Style",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontFamily: 'Noto Sans',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+                ),
+              ],
+            )
+          );
+        },
+      )
     );
   }
 }
-
